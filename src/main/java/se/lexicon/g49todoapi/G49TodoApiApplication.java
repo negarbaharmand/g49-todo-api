cc2bbe896ebb48a0ad7aed2e2b6908cec23c38cd
Negar Baharmand <negarbaharmand@outlook.com>
2024-06-11 09:33:45 +0200

    first commit

---
+++ src/main/java/se/lexicon/g49todoapi/G49TodoApiApplication.java
@@ -0,0 +1,13 @@
+package se.lexicon.g49todoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class G49TodoApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(G49TodoApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/lexicon/g49todoapi/G49TodoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.g49todoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class G49TodoApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
