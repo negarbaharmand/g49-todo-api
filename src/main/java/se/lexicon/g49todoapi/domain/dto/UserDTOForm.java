80f2172263c471744bf9b5a7b588d66d7b28145d
Negar Baharmand <negarbaharmand@outlook.com>
2024-06-11 12:02:09 +0200

    Add class diagram

--- src/main/java/se/lexicon/g49todoapi/domain/dto/UserDTOForm.java
+++ src/main/java/se/lexicon/g49todoapi/domain/dto/UserDTOForm.java
@@ -1,8 +1,17 @@
 package se.lexicon.g49todoapi.domain.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import se.lexicon.g49todoapi.domain.entity.Role;
 
 import java.util.Set;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 
 public class UserDTOForm {
     private String email;
