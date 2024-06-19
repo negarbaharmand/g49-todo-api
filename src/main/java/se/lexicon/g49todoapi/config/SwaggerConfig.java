60a5e0eabe6371402e160e6c3f32cc1038bc40c9
Negar Baharmand <negarbaharmand@outlook.com>
2024-06-19 10:48:00 +0200

    Add Spring Boot validations in the controller, exception handling class and Swagger documentation

---
+++ src/main/java/se/lexicon/g49todoapi/config/SwaggerConfig.java
@@ -0,0 +1,12 @@
+package se.lexicon.g49todoapi.config;
+
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(info = @Info(title = "TODO API", version = "0.1", description = "API Information"))
+public class SwaggerConfig {
+
+}
--- src/main/java/se/lexicon/g49todoapi/controller/UserController.java
+++ src/main/java/se/lexicon/g49todoapi/controller/UserController.java
@@ -1,9 +1,15 @@
 package se.lexicon.g49todoapi.controller;
 
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import se.lexicon.g49todoapi.domain.dto.RoleDTOView;
 import se.lexicon.g49todoapi.domain.dto.UserDTOForm;
@@ -16,6 +22,7 @@ import java.util.Set;
 
 @RequestMapping("/api/v1/users")
 @RestController
+@Validated
 public class UserController {
 
     private final UserService userService;
@@ -26,7 +33,12 @@ public class UserController {
     }
 
     @GetMapping
-    public ResponseEntity<UserDTOView> doGetUserByEmail(@RequestParam String email) {
+    public ResponseEntity<UserDTOView> doGetUserByEmail(
+            @RequestParam
+            @NotEmpty
+            @NotNull
+            @Pattern(regexp = "^[A-Za-z0-9+_.-]+@(.+)$", message = "Invalid email format") //simple email format
+            String email) {
         System.out.println(">>>>>>> getUserByEmail has been executed");
         System.out.println("Email: " + email);
 
@@ -34,20 +46,26 @@ public class UserController {
         return ResponseEntity.status(HttpStatus.OK).body(responseBody);
     }
 
+    @Operation(summary = "Registers a new user", description = "Creates a new user in the system")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "User created successfully"),
+            @ApiResponse(responseCode = "400", description = "Invalid input")
+    })
     @PostMapping
-    public ResponseEntity<UserDTOView> doRegister(@RequestBody UserDTOForm dtoForm) {
+    public ResponseEntity<UserDTOView> doRegister(@RequestBody @Valid UserDTOForm dtoForm) {
         System.out.println("DTO Form: " + dtoForm);
         UserDTOView responseBody = userService.register(dtoForm);
         return ResponseEntity.status(HttpStatus.CREATED).body(responseBody);
     }
 
     @PutMapping("/disable")
-    public ResponseEntity<Void> doDisableUserByEmail(@RequestParam String email){
+    public ResponseEntity<Void> doDisableUserByEmail(@RequestParam String email) {
         userService.disableEmail(email);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
+
     @PutMapping("/enable")
-    public ResponseEntity<Void> doEnableUserByEmail(@RequestParam String email){
+    public ResponseEntity<Void> doEnableUserByEmail(@RequestParam String email) {
         userService.enableEmail(email);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
